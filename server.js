bec2a75f9dc51059fd0a73570c7b5facac5fed10
Eric Peterson <ericmpeterson04@gmail.com>
2024-01-29 16:19:06 -0700

    Added more code

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 // Import inquirer, express and mysql modules
 const inquirer = require('inquirer');
+const mysql = require('mysql2/promise');
 const express = require('express');
-const mysql = require('mysql2');
 
-// Setup correct port
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-/*
+
 // Connect to database
 const db = mysql.createConnection(
     {
-      host: 'localhost',
+    host: 'localhost',
       // MySQL username,
       user: 'root',
       // TODO: Add MySQL password
       password: 'Goingtotown234',
       database: 'employees_db'
     },
-    console.log(`Connected to the employees_db database.`)
-  );
-*/
+    console.log(`Connected to the employees database.`)
+);
+
 const questions = [
     {
         type: 'list',
@@ -33,16 +32,22 @@ const questions = [
     },
 ];
 
-// Create a function to execute user selection
+function queryType(data) {
+    const choice = data.actions;
+    
+    // Case statement based off initial user input
+    switch (choice) {
+        case 'View all departments':
+            db.query('SELECT * FROM department', function (err, results) {
+                console.log(results);
+              });
+    };
+}
+ // Create a function to execute user selection
 function init() {
     inquirer
     .prompt(questions)
     .then(data => queryType(data));
 };
 
-function queryType(data) {
-    const choice = data.actions;
-    
-};
-
-init(); 
+init();
