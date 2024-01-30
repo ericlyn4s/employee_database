a0eadea86e84fecad28c6f6afd04603439186954
Eric Peterson <ericmpeterson04@gmail.com>
2024-01-29 20:22:22 -0700

    More adjustments

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ const questions = [
         name: 'actions',
         choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
     },
+    {
+        type: 'input',
+        message: 'give input',
+        name: 'yes',
+    },
 ];
 
 function queryType(data) {
@@ -31,25 +36,30 @@ function queryType(data) {
     switch (choice) {
         case 'View all departments':
             // Simple select * from departments table
-            db.query('SELECT * FROM department', function (err, results) {
-                console.log(results);
+            db.query('SELECT * Exclude (index) FROM department' , function (err, results) {
+                console.table(results);
               });
+            break;
         case 'View all roles':
             // Query role table for all values
             db.query('SELECT r.id, r.title, d.name, r.salary FROM role AS r JOIN department AS d ON r.department_id = d.id', function (err, results) {
-                console.log(results);
+                console.table(results);
               });
+            break;
         case 'View all employees':
-            // Query employee table for all values
-            db.query('SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, e.manager_id FROM employee AS e JOIN role AS r ON e.role_id = r.id JOIN department AS d ON r.department_id = d.id', function (err, results) {
-                console.log(results);
+            // Query employee table for all values, joining three additional tables to pull in the proper columns
+            db.query('SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, COALESCE(CONCAT(f.first_name," ",f.last_name), "null") AS manager FROM employee AS e JOIN role AS r ON e.role_id = r.id JOIN department AS d ON r.department_id = d.id LEFT JOIN employee AS f ON e.manager_id = f.id', function (err, results) {
+                console.table(results);
               });
+            break;
+        case 'Add a department':
+
     };
 }
  // Create a function to execute user selection
 function init() {
     inquirer
-    .prompt(questions)
+    .prompt(questions[0])
     .then(data => queryType(data));
 };
 
