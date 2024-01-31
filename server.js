6bd2e4c01770009c4cd391d231f2445a363f42b5
Eric Peterson <ericmpeterson04@gmail.com>
2024-01-31 16:38:37 -0700

    Got 6 of 7 queries working

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const initialQuestion = [
         type: 'list',
         message: 'What would you like to do?',
         name: 'actions',
-        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit application'],
     },
 ];
 
@@ -35,18 +35,21 @@ function queryType(data) {
             // Simple select * from departments table
             db.query('SELECT * FROM department' , function (err, results) {
                 console.table(results);
+                init();
               });
             break;
         case 'View all roles':
             // Query role table for all values
             db.query('SELECT r.id, r.title, d.name, r.salary FROM role AS r JOIN department AS d ON r.department_id = d.id', function (err, results) {
                 console.table(results);
+                init();
               });
             break;
         case 'View all employees':
             // Query employee table for all values, joining three additional tables to pull in the proper columns
             db.query('SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, COALESCE(CONCAT(f.first_name," ",f.last_name), "null") AS manager FROM employee AS e JOIN role AS r ON e.role_id = r.id JOIN department AS d ON r.department_id = d.id LEFT JOIN employee AS f ON e.manager_id = f.id', function (err, results) {
                 console.table(results);
+                init();
               });
             break;
         case 'Add a department':
@@ -63,14 +66,27 @@ function queryType(data) {
                     db.query('INSERT INTO department (name) VALUES (?)', [data.department], (error, result) => {
                         if (error) {
                             console.error('Error inserting record:', error);
+                            init();
                         } else {
                             console.log(`${data.department} inserted successfully!`);
+                            init();
                         }
                     });
                });
             break;
         case 'Add a role':
             // Prompt the user for the necessary details to add a role
+            let departmentNames = [];
+            db.query('SELECT name from department', (error, results) => {
+                if (error) {
+                    throw error;
+                } 
+                results.forEach((department, i) => {
+                    departmentNames.push({
+                        name: department.name,
+                        value: i+1,
+                    })
+                })
             inquirer
             .prompt([
              {
@@ -84,9 +100,10 @@ function queryType(data) {
                 message: 'What is the salary?',
             },
             {
-                type: 'input',
+                type: 'list',
                 name: 'department',
-                message: 'What is the department id?',
+                message: 'What is the department?',
+                choices: departmentNames,
             },
              ])
              // Take user input and input it into a mysql query
@@ -94,13 +111,38 @@ function queryType(data) {
                  db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [data.title, data.salary, data.department], (error, result) => {
                      if (error) {
                          console.error('Error inserting record:', error);
+                         init();
                      } else {
                          console.log(`Role added successfully!`);
+                         init();
                      }
                  });
                 });
+            });   
             break;
         case 'Add an employee':
+            let roleId = [];
+            let employeeName = [];
+            db.query('SELECT title FROM role ', (error, results) => {
+                if (error) {
+                    throw error;
+                } 
+                results.forEach((role, i) => {
+                    roleId.push({
+                        name: role.title,
+                        value: i+1,
+                    })
+                })
+                db.query('SELECT CONCAT(first_name," ",last_name) AS name FROM employee', (error, results) => {
+                    if (error) {
+                        throw error;
+                    } 
+                    results.forEach((employee, i) => {
+                        employeeName.push({
+                            name: employee.name,
+                            value: i+1,
+                        })
+                    })
             // Prompt the user for the necessary details to add an employee
             inquirer
             .prompt([
@@ -115,14 +157,16 @@ function queryType(data) {
                 message: 'What is the last name of the employee?',
             },
             {
-                type: 'input',
+                type: 'list',
                 name: 'role',
-                message: "What is the employee's role id?",
+                message: 'What is the role?',
+                choices: roleId,
             },
             {
-                type: 'input',
+                type: 'list',
                 name: 'manager',
-                message: "What is the employee's manager's id? (Input null if no manager)",
+                message: "What is the employee's manager's id?",
+                choices: employeeName,
             },           
              ])
              // Take user input and input it into a mysql query
@@ -130,11 +174,15 @@ function queryType(data) {
                  db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [data.first, data.last, data.role, data.manager], (error, result) => {
                      if (error) {
                          console.error('Error inserting record:', error);
+                         init();
                      } else {
                          console.log(`Employee added successfully!`);
+                         init();
                      }
                  });
                 });
+            });
+        });
             break;
             case 'Update an employee role':
                 // Prompt the user for the necessary details to add an employee
@@ -157,8 +205,11 @@ function queryType(data) {
                     .then((data) => {
                         // I need to update this, then revert back to what would we like to do
                      console.log(data.employees);
+                     init();
                     })  
                 break;   
+                default: 
+                    process.exit();
                 } 
 };
 
